c62d996157840e9eed1a0efac5997f848173e1cb
Lukas Peterson <lukas.peterson@ibm.com>
2019-12-03 12:11:27 -0800

    refactor: Update welcome message

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! I\'m your Bikeshare Analytics App. Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     cities = ['chicago', 'Chicago', 'New York City', 'new york city', 'washington', 'Washington']
     months = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
